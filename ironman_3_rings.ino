0db78c678ce7e2f14d0b48b71910be5e0497952e
Eugen Martin <eugeniusmartinus@gmail.com>
2016-08-22 21:41:22 +0200

    minor changes

--- ironman_3_rings.ino
+++ ironman_3_rings.ino
@@ -6,9 +6,9 @@
 #include <avr/power.h>
 #endif
 
-#define PIN 0
+#define PIN 1
 #define SWITCH 2
-#define PIX 47
+#define PIX 16
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(PIX, PIN);
 
@@ -22,9 +22,6 @@ uint8_t innerFrom = 40,
         outerFrom = 0,
         outerTo = 23;
 
-int program = 0;
-int programs = 5;
-
 void setup() {
 
 #ifdef __AVR_ATtiny85__ // Trinket, Gemma, etc.
@@ -32,55 +29,56 @@ void setup() {
 #endif
 
   pixels.begin();
-  pixels.setBrightness(50); // 1/3 brightness
-  //pixels.setBrightness(240); // 1/3 brightness
+  pixels.setBrightness(32); // 1/3 brightness
   randomSeed(analogRead(0));
-  //program = random(programs);
-  
+
   // initialize the SWITCH pin as an input.
   pinMode(SWITCH, INPUT);
   // ...with a pullup
-  digitalWrite(SWITCH, HIGH);  
+  digitalWrite(SWITCH, HIGH);
 }
 
 uint32_t onColor = pixels.Color(250, 120, 60);
 uint32_t black = pixels.Color(0, 0, 0);
 
-int frame = 5; //ms
+int frame = 25; //ms
 
-
-uint32_t loops = 0;
-int maxLoops = 100;
+int program = 3;
+int programs = 5;
 
 void loop() {
-  //randomDot();
-  //randMatrix();
-   return heartBeat();
-  // rotatingRings();
+  // randomDot();
+  // randMatrix();
+  // return heartBeat();
+  // return rotatingRings();
   // return clock();
-  
+
   if (!digitalRead(SWITCH)) {
     program = (program + 1) % programs;
   }
-  
-  
+
+
   switch ( program ) {
     case 0:
-      randomDot();
-      break;
-    case 1:
-      randomMatrix();
-      break;
-    case 2:
       heartBeat();
       break;
-    case 3:
+    case 1:
       rotatingRings();
+      break;
+    case 2:
+      randomDot();
+      break;
+    case 3:
+      randomMatrix();
       break;
     case 4:
       clock();
       break;
   }
+}
+
+void ironMan() {
+
 }
 
 int rotInner = 0,
@@ -92,12 +90,12 @@ int rotInner = 0,
 
 void clock() {
   rotateOuter();
-  
-  if(rotOuter == 0) {
+
+  if (rotOuter == 0) {
     rotateMiddle();
   }
-  if(rotMiddle == 0){
-    rotateInner();     
+  if (rotMiddle == 0) {
+    rotateInner();
   }
   delay(100);
 }
@@ -126,14 +124,15 @@ void rotateMiddle() {
 void rotateInner() {
   int innerTotal = innerTo - (innerFrom + 1);
   setColor(innerFrom, innerTo, black);
-  setRingColor((innerFrom + 1), innerTotal, rotInner, lenInner, onColor);
+  //  setRingColor((innerFrom + 1), innerTotal, rotInner, lenInner, onColor);
+  setColor(innerFrom - 1, innerTo, onColor);
   rotInner = (rotInner + 1) % innerTotal;
 }
 
 void setRingColor(int ringStart, int ringTotal, int stripeStart, int stripeLength, uint32_t color) {
   uint8_t  i;
   for (i = stripeStart; i <= (stripeStart + stripeLength); i++) {
-    pixels.setPixelColor(ringStart + i % (ringTotal+1), color);
+    pixels.setPixelColor(ringStart + i % (ringTotal + 1), color);
   }
   pixels.show();
 }
@@ -168,21 +167,21 @@ int pulse = 1100; //ms
 
 void heartBeat() {
   frame = 35;
-  beat(frame);
+  beat(frame, onColor);
   delay(2 * frame);
-  beat(frame);
+  beat(frame, onColor);
   delay(pulse - frame * 16);
 }
 
-void beat(int frame) {
-  setColor(innerFrom, innerTo, onColor);
+void beat(int frame, uint32_t color) {
+  setColor(innerFrom, innerTo, color);
   delay(frame);
-  setColor(middleFrom, middleTo, onColor);
+  setColor(middleFrom, middleTo, color);
   delay(frame);
-  setColor(outerFrom, outerTo, onColor);
+  setColor(outerFrom, outerTo, color);
   delay(2 * frame);
 
-  setColor(outerFrom, outerTo, black);
+  setColor(outerFrom, outerTo, color);
   delay(frame);
   setColor(middleFrom, middleTo, black);
   delay(frame);
